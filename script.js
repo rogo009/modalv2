dea39f01c36e64682412c857407017b791e1c0a0
robin <rogo009git@gmail.com>
2020-03-02 19:58:26 -0800

    update close modal function

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 const modal = document.getElementById('modal');
 // get open modal button
 const modalBtn = document.getElementById('modal-button');
+// get close modal button
+const closeBtn = document.getElementById('close-button');
 
 modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
 
 function openModal() {
     modal.style.display = "block";
 }
+
+function closeModal() {
+    modal.style.display = "none";
+} 
